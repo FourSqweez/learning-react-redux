a5240364a57413fd1af8ddee0fec64f5979e5d3f
FourSqweez <four7518@gmail.com>
2021-03-03 15:18:08 +0700

    Added : ItemsContainer to reuse components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CakeContainer from './components/CakeContainer'
 import HookCakeContainer from './components/HookCakeContainer'
 import HookIceCreamContainer from './components/HookIceCreamContainer'
 import IceCreamContainer from './components/IceCreamContainer'
+import ItemsContainer from './components/ItemsContainer'
 import NewCakeContainer from './components/NewCakeContainer'
 import store from './redux/store'
 
@@ -11,11 +12,13 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemsContainer cake />
+        <ItemsContainer iceCream/>
         <CakeContainer />
         <HookCakeContainer />
         <IceCreamContainer />
         <HookIceCreamContainer />
-        <NewCakeContainer/>
+        <NewCakeContainer />
       </div>
     </Provider>
   )
